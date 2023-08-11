198bef6029a6dc4ed3bf41d004cea130d90ecb96
Isaac Peruga Ortiga <isaacperuga03@gmail.com>
2023-08-11 16:48:26 +0200

    Start with exercise 3

--- src/main/kotlin/Ejercicio_3.kt
+++ src/main/kotlin/Ejercicio_3.kt
@@ -1,3 +1,26 @@
-fun main(){
+fun main() {
+    print("Enter the initial type of temperature: ")
+    val typeTemperatureInitial = readln().toString()
+    println("Put the value of the temperature: ")
+    val valueTemperature = readln().toInt()
+    print("Enter a type of temperature to converse that: ")
+    val typeTemperatureFinal = readln().toString()
 
+    /*when(typeTemperatureFinal){
+        "Kelvin" -> printFinalTemperature(valueTemperature,typeTemperatureInitial,typeTemperatureFinal)
+        "Celsius" -> printFinalTemperature()
+        "Fahrenheit" ->
+        else -> println("That type is not avalable. Temperatures avalables: Kelvin, Celsius, Fahrenheit ")
+
+    }*/
+}
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
 }
