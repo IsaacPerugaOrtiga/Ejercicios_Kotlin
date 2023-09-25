daa3a69e7a70dc1068dfd7985aa7db97a987d17d
Isaac Peruga Ortiga <isaacperuga03@gmail.com>
2023-09-25 21:49:03 +0200

    Ejercicio 5 terminado.

---
+++ src/main/kotlin/Ejercicio_5/Ejercicio_5.kt
@@ -0,0 +1,11 @@
+package Ejercicio_5
+
+
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+
+}
